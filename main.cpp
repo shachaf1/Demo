408f7fbd26d8635bb8d7616cdde50dce78a0d803
Billy Everyteen <your_email@example.com>
2021-10-26 13:36:02 -0700

    stam

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+
+    int a = 12;
+    std::cin >>a;
+    std::string string;
+    std::cin >>string;
+    std::cout << "Hello, World!" << a << " ->->" << string << std::endl;
+    return 0;
+}
